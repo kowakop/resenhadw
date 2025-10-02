68fd82312821982e456c4e3bac4f985f724f3fc0
SunFellings <pedrhenriquesantos2008@gmail.com>
2025-10-02 14:48:25 -0300

    uns bagui ai

---
+++ cadastro/cadastro_usuario.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="saida.php" method='POST'>
+    Nome: <br>
+    <input type="text" name="nome">
+    <br><br>
+
+    Data de nascimento:
+    <br>
+    <input type="text" name="data">
+    <br><br>
+
+    E-mail:
+    <br>
+    <input type="text" name="email">
+    <br><br>
+    
+    Digite sua senha:
+    <br>
+    
+    <input type="text" name="senha">
+    <br><br>
+    
+    Selecione sua foto de perfil:
+    <br>
+    <input type="" name="foto">
+<br><br>
+    <input type="submit" value="Salvar">
+    </form>
+</body>
+</html>
---
+++ login/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="salvar_autor.php" method=''>
+    Nome de usuário ou E-mail: <br>
+    <input type="text" name="nome">
+    <br><br>
+
+    Senha:
+    <br>
+    <input type="text" name="senha">
+    <br><br>
+    <input type="submit" value="Salvar">
+    </form>
+
+</body>
+</html>
