4e01ad9740e0788ed9248cc124458ff419547fcf
Kowa <kibecomkibe@gmail.com>
2025-10-07 10:18:46 -0300

    index

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resenhando Mangás</title>
+    <title>Document</title>
 </head>
 <body>
-
-    <a href="./login">Login</a> <br>
-    <a href="./cadastro/cadastro_usuario.php">Cadastrar</a>
-
+    <a href="./login">Logar</a> <br>
+    <a href="./autor">Autor</a> <br>
+    <a href="./cadastro/cadastro_usuario.php">Cadastro</a> <br>
+    <a href="./obra">Obras</a> <br>
 </body>
 </html>
