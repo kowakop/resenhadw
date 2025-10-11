86bfddafc6cba9d4a675931543c10b96cc5b19db
GustavoTsu <gustavosergio869@gmail.com>
2025-10-11 08:28:43 -0300

    banco-cadastro

--- cadastro/saida.php
+++ cadastro/saida.php
@@ -1,11 +1,36 @@
 <?php
 
-    require_once "conexao.php";
+    require_once "../conexao.php";
 
     $nome = $_POST['nome'];
     $nascimento = $_POST['nascimento'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
+
+    $nome = trim($nome);
+    $email = trim($email);
+    $senha = trim($senha);
+    $nascimento = trim($nascimento);
+
+    if (strlen($email) > 40) {
+        header("Location: index.php");
+    }
+    
+    if (strlen($nome) > 14) {
+        header("Location: index.php");
+    }
+    
+    if (strlen($senha) > 30) {
+        header("Location: index.php");
+    }
+    
+    if ($nome == "" || $email == "" || $senha == "" || $nascimento == "") {
+        header("Location: index.php");
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        header("Location: index.php");
+    }
 
     $nome_arquivo = $_FILES['foto']['name'];
     $caminho_temporario = $_FILES['foto']['tmp_name'];
@@ -17,18 +42,18 @@
     $novo_nome = uniqid() . "." . $extensao;
 
     // lembre-se de criar a pasta e de ajustar as permissões.
-    $caminho_destino = "fotos/" . $novo_nome;
+    $caminho_destino = "../fotos/" . $novo_nome;
 
     // move a foto para o servidor
     move_uploaded_file($caminho_temporario, $caminho_destino);
 
 
-    $sql = "INSERT INTO tb_usuario (usuario_nome, usuario_data_nasc, usuario_email, usuario_senha, usuario_foto) 
+    $sql = "INSERT INTO usuario (usuario_nome, usuario_data_nasc, usuario_email, usuario_senha, usuario_foto) 
     VALUES (?, ?, ?, ?, ?)";
 
     $comando = mysqli_prepare($conexao, $sql);
     
-    mysqli_stmt_bind_param($comando, 'sssss', $nome, $nascimento, $email, $senha);
+    mysqli_stmt_bind_param($comando, 'sssss', $nome, $nascimento, $email, $senha, $novo_nome);
 
     mysqli_stmt_execute($comando);
 
