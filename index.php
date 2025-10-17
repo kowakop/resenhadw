4d459fb5c6755e08414974f2fff1bd7942f5288d
GustavoTsu <gustavosergio869@gmail.com>
2025-10-17 10:33:55 -0300

    favicon3

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resenhando Mangás</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
     <a href="./login/">Logar</a> <br>
