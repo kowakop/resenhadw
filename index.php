2128290d8eafae58f856d1b5734ac2d5d626146b
GustavoTsu <gustavosergio869@gmail.com>
2025-11-04 09:21:32 -0300

    algumas mudanças no git

--- index.php
+++ index.php
@@ -67,20 +67,30 @@ if (!isset($_SESSION['id'])) {
     <!-- MENU à DIREITA -->
     <div id="base_menu">
         <div id="logo_menu">
-            <a href="index.php" target="principal">
+            <a href="resenha/feed.php" target="principal">
                 <img src="./fotos/logo.png" alt="logo Resenhando Mangás" id="logo" style="height: 70px;">
                 <img src="./fotos/Resenhando.png" alt="texto resenhando mangás" id="logo2" style="height: 80px; width: 250px;">
             </a>
         </div>
 
-        <a href="index.php" class="link_menu" target="principal">
-            🏠 <span class="texto_menu">Home</span>
+        <a href="resenha/feed.php" class="link_menu" target="principal">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-home icon" viewBox="0 0 24 24" style="color: currentcolor;">
+                    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                    <path d="M9 22V12h6v10"></path>
+                </svg>
+            <span class="texto_menu">Home</span>
         </a>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" class="icon" style="color: currentcolor;">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 21-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+        </svg> Salvos blabla adicionar aqui autores resenhas e resenhistas salvos ou seja mais tres links
+
+
         <a href="./listar.php?objeto=obra" class="link_menu" target="principal">
             📖 <span class="texto_menu">Obras</span>
         </a>
         <a href="./listar.php?objeto=resenha" class="link_menu" target="principal">
-            📝 <span class="texto_menu">Resenhas</span>
+            📝 <span class="texto_menu">Todas as resenhas</span>
         </a>
         <a href="./listar.php?objeto=autor" class="link_menu" target="principal">
             👨‍🎨 <span class="texto_menu">Autores</span>
